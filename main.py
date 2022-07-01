281d41bb983d2f19834f87037eb23c38976fb7f8
AbdElrahman Eid <AbdElrahman.Eid97@gmail.com>
2022-07-02 00:25:25 +0200

    Dashboard Initial Design

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from turtle import width
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 import dash_daq as daq
+import pandas as pd
+import plotly.express as px
 
 app = Dash(
     external_stylesheets=[
@@ -12,44 +13,79 @@ app = Dash(
     ]
 )
 
+df = pd.read_csv('/home/yinshe/Documents/PythonZone/DataViz/ExplanationProject/penguins_size.csv')
+scatter_fig = px.scatter(df, x='culmen_length_mm', y='flipper_length_mm', color='species', template='simple_white', height=750)
+scatter_fig.update_layout({
+    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    'legend_bgcolor': 'rgba(0, 0, 0, 0)',
+})
 app.layout = html.Div([
     dbc.Row([
             dbc.Col(dbc.Row([
                 dbc.Col([
                     html.H5('F1 Score'),
-                    dbc.CardBody('95.7%', className='bans')
-                ], width=2),
+                    # dbc.CardBody('95.7%', className='bans')
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Adelie')
+                        ], width=3),
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Chinstrap')
+                        ], width=3),
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Gentoo')
+                        ], width=3),
+                    ], className='justify-content-evenly'),
+                ], width=4),
                 dbc.Col([
                     html.H5('Recall'),
-                    dbc.CardBody('85.4%', className='bans')
-                ], width=2),
+                    # dbc.CardBody('95.7%', className='bans')
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Adelie')
+                        ], width=3),
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Chinstrap')
+                        ], width=3),
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Gentoo')
+                        ], width=3),
+                    ], className='justify-content-evenly'),
+                ], width=4),
                 dbc.Col([
                     html.H5('Precision'),
-                    dbc.CardBody('90.5%', className='bans')
-                ], width=2),
-                dbc.Col([
-                    html.H5('Accuracy'),
-                    dbc.CardBody('98.3%', className='bans')
-                ], width=2)
-            ], className='justify-content-evenly'), width=12)
+                    # dbc.CardBody('95.7%', className='bans')
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Adelie')
+                        ], width=3),
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Chinstrap')
+                        ], width=3),
+                        dbc.Col([
+                            dbc.CardBody('95.7%', className='bans Gentoo')
+                        ], width=3),
+                    ], className='justify-content-evenly'),
+                ], width=4),
+            ], className='justify-content-evenly overlay'), width=12)
         ], style={'color': 'white'}),
     dbc.Row(
         [
             dbc.Col(
+                dcc.Graph(figure=scatter_fig)
+            , width=6),
+            dbc.Col(
                 dbc.Row([
-                    daq.ToggleSwitch(
-                        label='My toggle switch',
-                        labelPosition='bottom'
-                    )
-                ], className='overlay')
-            , width=6, id='overlay_col'),
-            dbc.Col(html.Div("Two of three columns"), width=6),
-        ]
-    ),
-    
-    daq.BooleanSwitch(id='my-boolean-switch', on=False),
-    
 
+                ]),
+                dbc.Row([
+
+                ]),
+            )
+        ]
+    , className='overlay figs')
+    
 ], className='container-fluid')
 
 if __name__ == "__main__":
